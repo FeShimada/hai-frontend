66de4fc603ee4aba8c30b4b9bfe51a79b0da78ef
felipe <felipe.shimada@kepha.com.br>
2023-08-21 11:52:28 -0300

    melhorando navbar mobile

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,73 +8,69 @@ interface NavbarProps {
   path: string;
 }
 
-const useOutsideClick = (ref: React.RefObject<HTMLDivElement>, onClickOutside: () => void) => {
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (ref.current && !ref.current.contains(event.target as Node)) {
-        onClickOutside();
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [ref, onClickOutside]);
-};
-
 const Navbar: React.FC<NavbarProps> = ({ route, path }) => {
   const [clicked, setClicked] = useState(false);
-  const navRef = useRef<HTMLDivElement>(null); // referencia objeto mutavel o current pode ser usada para armazenar valores mutáveis que persistirão entre as renderizações do componente.
+  const navRef = useRef<HTMLDivElement>(null);
 
   const handleToggleSidebar = () => {
     setClicked(!clicked);
   };
 
-  const handleSidebarClose = () => {
-    setClicked(false);
-  };
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (navRef.current && !navRef.current.contains(event.target as Node)) {
+        setClicked(false);
+      }
+    };
 
-  useOutsideClick(navRef, handleSidebarClose);
+    if (clicked) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [clicked]);
 
   return (
-    <nav>
+    <nav ref={navRef}>
       <NavLink to={route}>
         <img src={logo} alt="Logo da empresa" />
       </NavLink>
 
-      {/* queremos referenciar essa div, para manipula-la */}
-      <div ref={navRef}> 
+      <div >
         <ul id="navbar" className={clicked ? "active" : ""}>
           <li>
-            <NavLink to="/" onClick={handleSidebarClose}>
+            <NavLink to="/" onClick={handleToggleSidebar}>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to={"/produtos"} onClick={handleSidebarClose}>
+            <NavLink to={"/produtos"} onClick={handleToggleSidebar}>
               Produtos
             </NavLink>
           </li>
           <li>
-            <NavLink to={`${route}/index.html`} onClick={handleSidebarClose}>
+            <NavLink to={`${route}/index.html`} onClick={handleToggleSidebar}>
               Blog
             </NavLink>
           </li>
           <li>
-            <NavLink to={`${route}/index.html`} onClick={handleSidebarClose}>
+            <NavLink to={`${route}/index.html`} onClick={handleToggleSidebar}>
               About
             </NavLink>
           </li>
           <li>
-            <NavLink to={`${route}/index.html`} onClick={handleSidebarClose}>
+            <NavLink to={`${route}/index.html`} onClick={handleToggleSidebar}>
               Contact
             </NavLink>
           </li>
         </ul>
       </div>
 
-      <div id="mobile">
+      <div id="mobile" >
         <i
           id="bar"
           className={clicked ? "fas fa-times" : "fas fa-bars"}
